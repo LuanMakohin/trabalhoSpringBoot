164a40c96426f43b99365fcf6c2784fe38d75372
LuanMakohin <luan0089@hotmail.com>
2023-07-12 20:19:07 -0300

    Finalizacao

--- app/src/main/java/mensalidades/app/Controller/JogadorController.java
+++ app/src/main/java/mensalidades/app/Controller/JogadorController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import mensalidades.app.model.Jogador;
 import mensalidades.app.repository.JogadorRepository;
 
+@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "Content-Type")
 @RestController
 @RequestMapping("/api")
 public class JogadorController {
--- app/src/main/java/mensalidades/app/Controller/MensalidadeController.java
+++ app/src/main/java/mensalidades/app/Controller/MensalidadeController.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import mensalidades.app.model.Mensalidade;
 import mensalidades.app.repository.JogadorRepository;
 import mensalidades.app.repository.MensalidadeRepository;
 
+@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "Content-Type")
 @RestController
 @RequestMapping("/api")
 public class MensalidadeController {
